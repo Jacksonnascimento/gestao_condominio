f8e185b975f38d0f89ad3b368a4542cecd7fe4c4
Jackson Nascimento <44805133+Jacksonnascimento@users.noreply.github.com>
2025-07-06 01:06:28 -0300

    RequestLoggingFilter

--- api/src/main/java/br/com/gestaocondominio/api/security/RequestLoggingFilter.java
+++ api/src/main/java/br/com/gestaocondominio/api/security/RequestLoggingFilter.java
@@ -7,30 +7,42 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 
 @Component
 public class RequestLoggingFilter extends OncePerRequestFilter {
 
     @Override
-    protected void doFilterInternal(
-            @NonNull HttpServletRequest request,
-            @NonNull HttpServletResponse response,
-            @NonNull FilterChain filterChain)
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
             throws ServletException, IOException {
+        
+       
+        ContentCachingRequestWrapper wrappedRequest = new ContentCachingRequestWrapper(request);
 
         System.out.println("--- [LOGGING FILTER]: INCOMING REQUEST ---");
-        System.out.println("Method: " + request.getMethod());
-        System.out.println("URI: " + request.getRequestURI());
+        System.out.println("Method: " + wrappedRequest.getMethod());
+        System.out.println("URI: " + wrappedRequest.getRequestURI());
 
         System.out.println("Headers: ");
-        Collections.list(request.getHeaderNames()).forEach(headerName ->
-                System.out.println(headerName + ": " + Collections.list(request.getHeaders(headerName)))
+        Collections.list(wrappedRequest.getHeaderNames()).forEach(headerName ->
+                System.out.println(headerName + ": " + Collections.list(wrappedRequest.getHeaders(headerName)))
         );
-        System.out.println("-----------------------------------------");
         
-        filterChain.doFilter(request, response);
+        
+        filterChain.doFilter(wrappedRequest, response);
+
+      
+        byte[] requestBody = wrappedRequest.getContentAsByteArray();
+        if (requestBody.length > 0) {
+            String bodyString = new String(requestBody, StandardCharsets.UTF_8);
+            System.out.println("Request Body: " + bodyString);
+        } else {
+            System.out.println("Request Body: [EMPTY]");
+        }
+        System.out.println("-----------------------------------------");
     }
 }
